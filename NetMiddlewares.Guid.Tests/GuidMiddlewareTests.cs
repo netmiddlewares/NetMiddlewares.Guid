82f6fa4716706d4b65428a4585ba18c63057a461
Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
2020-01-03 01:07:28 -0300

    Added test project

---
+++ NetMiddlewares.Guid.Tests/GuidMiddlewareTests.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetMiddlewares.Guid.Tests
+{
+    public class GuidMiddlewareTests
+    {
+        [Fact]
+        public async Task TestGuidMiddlewareWorksCorrectly()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
